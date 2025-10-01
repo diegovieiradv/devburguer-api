50f1f0e61509c15cbde8caaa7ddae3fe8f0c4895
diegovieiradv <dvdiego87@gmail.com>
2025-09-30 23:47:19 -0300

    adicionei a pasta models ao project

---
+++ src/app/models/User.js
@@ -0,0 +1,18 @@
+import Sequelize, { Model } from "sequelize";
+
+class User extends Model {
+  static init(sequelize) {
+    super.init(
+      {
+        name: sequelize.STRING,
+        email: sequelize.STRING,
+        password_hash: sequelize.STRING,
+        admin: sequelize.BOOLEAN,
+      },
+      {
+        sequelize,
+      }
+    );
+  }
+}
+export default User;
---
+++ src/database/index.js
@@ -0,0 +1,9 @@
+import { Sequelize } from "sequelize";
+
+import User from "../app/models/user.js";
+
+class Database {
+  constructor() {
+    this.init();
+  }
+}
