305b98189bee6ded9bf0dd86a0290277826e857b
diegovieiradv <dvdiego87@gmail.com>
2025-10-06 21:38:25 -0300

    adicionei novos componetnts ao project

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,3 +1,45 @@
+import { v4 } from "uuid";
+import * as Yup from "yup";
+
+import User from "../models/user";
+
 class UserController {
-  async store(request, body) {}
+  async store(request, response) {
+    const schema = Yup.object({
+      name: Yup.string().required(),
+      email: Yup.string().email().required(),
+      password_hash: Yup.string().required().min(6),
+      admin: Yup.boolean(),
+    });
+
+    try {
+      schema.validateSync(request.body, { abortEarly: false });
+    } catch (err) {
+      return response.status(400).json({ error: err.errors });
+    }
+    const { name, email, password_hash, admin } = request.body;
+
+    const userExists = await User.findOne({ where: { email } });
+
+    if (userExists) {
+      return response.status(400).json({ error: "User already exists." });
+    }
+
+    const user = await User.create({
+      id: v4(),
+      name,
+      email,
+      password_hash,
+      admin,
+    });
+
+    return response.status(201).json({
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      admin: user.admin,
+    });
+  }
 }
+
+export default new UserController();
--- src/routes.js
+++ src/routes.js
@@ -1,18 +1,8 @@
 import { Router } from "express";
-import { v4 } from "uuid";
-
-import User from "./app/models/user";
+import UserController from "./app/controllers/UserController";
 
 const routes = new Router();
 
-routes.get("/", async (request, response) => {
-  const user = await User.create({
-    id: v4(),
-    name: "Diego",
-    email: "diegodv2@gmail.com",
-    password_hash: "123456",
-  });
-  return response.status(201).json(user);
-});
+routes.post("/users", UserController.store);
 
 export default routes;
